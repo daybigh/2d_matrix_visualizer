594ce3cab3d2add1c8b394dcf410c5c909f8fb46
darren kim <darrenkim2036@gmail.com>
2022-05-20 19:44:20 -0400

    Changed from lines to dots

--- index.js
+++ index.js
@@ -1,74 +1,101 @@
 var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d'); 
+const u = 4
 
-canvas.width = 1920;
+canvas.width = 1900;
 canvas.height = 800;
 
-var c = canvas.getContext('2d'); 
+let l = 300
 
-// backround grid
+ctx.translate(canvas.width / 2, canvas.height / 2)
 
-for (var i = -100; i < 100; i++) {
-    c.beginPath();
-    c.moveTo(40 * i, 0);
-    c.lineTo(40 * i, window.innerHeight);
-    c.strokeStyle = "lightgray"
-    c.stroke();
-};
+function drawSpan(vector, offset, c) {
+    ctx.beginPath()
+    ctx.moveTo(0 + offset[0] , -(0 + offset[1]))
+    ctx.lineTo(l * vector[0] + offset[0], -(l * vector[1]) - offset[1])
+    if (c == 1) {
+        ctx.strokeStyle = "cyan"
+    }
+    ctx.stroke()
 
-for (var i = -100; i < 100; i++) {
-    c.beginPath();
-    c.moveTo(0, 40 * i);
-    c.lineTo(window.innerWidth , 40 * i);
-    c.strokeStyle = "lightgray"
-    c.stroke();
-};
+    ctx.beginPath()
+    ctx.moveTo(0 + offset[0] , -(0 + offset[1]))
+    ctx.lineTo(-(l * vector[0]) + offset[0], l * vector[1] - offset[1])
+    if (c == 1) {
+        ctx.strokeStyle = "cyan"
+    }
+    ctx.stroke()
+}
 
-// where ihat and jhat get mapped to
+function matrixMult(matrix, vector) {
+    return [vector[0] * matrix[0][0] + vector[1] * matrix[0][1], vector[0] * matrix[1][0] + vector[1] * matrix[1][1]]
+}
 
+m = [
+    [2, 1], 
+    [2, 2]
+    ]
 
+let t = 80
 
-// x-axis
-// for (var i = -100; i < 200; i++) {
-//     c.beginPath();
-//     c.moveTo(0, 40 * i);
-//     c.lineTo(window.innerWidth , 40 * i);
-//     c.strokeStyle = "rgb(64, 136, 245)";
-//     c.stroke();
+// Base grid
+// for (var i = 0; i < t; i++) {
+//     for (var j = 0; j < t; j++) {
+//         drawSpan([1, 0], [u * i, u * j], 0)
+//         drawSpan([0, 1], [u * i, u * j], 0)
+//     }
 // }
 
-// c.beginPath();
-// c.translate(canvas.width / 2, canvas.height / 2);
-// c.rotate(Math.PI * (0));
-// c.moveTo(-(canvas.width / 2), 0);
-// c.lineTo((canvas.width / 2), 0);
-// c.strokeStyle = "rgb(64, 136, 245)";
-// c.stroke();
 
-for (var i = -100; i < 200; i++) {
-    c.beginPath();
-    c.translate(canvas.width / 2, 40 * i);
-    c.rotate(Math.PI * (0.2));
-    c.translate(-canvas.width / 2, -40 * i)
-    c.moveTo(-(canvas.width) * 2, 40 * i);
-    c.lineTo((canvas.width) * 2, 40 * i);
-    c.strokeStyle = "rgb(64, 136, 245)";
-    c.stroke();
-    c.closePath();
-    c.setTransform(1, 0, 0, 1, 0, 0)
-};
+//base dots
+for (var i = -t; i < t; i++) {
+    for (var j = -t; j < t; j++) {
+        ctx.fillRect(u * i, -u * j, 2, 2)
+    }
+}
 
-for (var i = -100; i < 200; i++) {
-    c.beginPath();
-    c.translate(40 * i, canvas.height / 2);
-    c.rotate(Math.PI * (0.2));
-    c.translate(-40 * i, -canvas.height / 2)
-    c.moveTo(40 * i, -canvas.height * 2);
-    c.lineTo(40 * i, canvas.height * 2);
-    c.strokeStyle = "rgb(64, 136, 245)";
-    c.stroke();
-    c.closePath();
-    c.setTransform(1, 0, 0, 1, 0, 0)
-};
+//transformed dots
+for (var i = -t; i < t; i++) {
+    for (var j = -t; j < t; j++) {
+        let vec = matrixMult(m, [i, j])
+        ctx.fillStyle = "red"
+        ctx.fillRect(u * vec[0], -u * vec[1], 2, 2)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (var i = -4; i < 5; i++) {
+//     ctx.beginPath();
+//     ctx.moveTo(- canvas.width / 4, u * i)
+//     ctx.lineTo(canvas.width / 4, u * i)
+//     ctx.stroke()
+// }
+
+// for (var i = -6; i < 7; i++) {
+//     ctx.beginPath();
+//     ctx.moveTo(u * i, -canvas.height / 4)
+//     ctx.lineTo(u * i, canvas.height / 4)
+//     ctx.stroke();
+// }
+
+
+
 
 
 
