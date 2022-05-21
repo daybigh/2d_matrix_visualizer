804f344f6809601368ed3050306fbcbaaac12c21
darren kim <darrenkim2036@gmail.com>
2022-05-21 02:57:01 -0400

    added determinants

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d'); 
-const u = 40
+const u = 45
 let t = 20
+
+ctx.globalCompositeOperation = 'destination-out'
 
 canvas.width = 20 * u;
 canvas.height = 20 * u;
@@ -12,6 +14,20 @@ function matrixMult(matrix, vector) {
     return [vector[0] * matrix[0][0] + vector[1] * matrix[0][1], vector[0] * matrix[1][0] + vector[1] * matrix[1][1]]
 }
 
+function displaySpan(v) {
+    ctx.beginPath()
+    ctx.lineTo(0, 0)
+    ctx.lineTo( 1000 * v[0], 1000 * v[1])
+    ctx.strokeStyle = 'rgb(32, 246, 0)'
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.lineTo(0, 0)
+    ctx.lineTo(-1000 * v[0], -1000 * v[1])
+    ctx.strokeStyle = 'rgb(32, 246, 0)'
+    ctx.stroke()
+
+}
 
 function displayBaseGrid() {
     //base vertical
@@ -47,8 +63,42 @@ function displayBaseGrid() {
         }
 }
 
+function getEigenValue (m) {
+    a = m[0][0]
+    b = m[0][1]
+    c = m[1][0]
+    d = m[1][1]
 
+    if ((a + d) ** 2 - 4 * (a * d - b * c) < 0) {
+        return null
+    } else {
+        temp1 = (a + d + Math.sqrt((a + d) ** 2 - 4 * (a * d - b * c))) / 2
+        temp2 = (a + d - Math.sqrt((a + d) ** 2 - 4 * (a * d - b * c))) / 2
+        return [temp1, temp2]
+    }
 
+}
+
+function getEigenVector(m) {
+    if (getEigenValue(m) == null) {
+        return null
+    } else {
+        eValue = getEigenValue(m)
+        temp1 = [
+            [m[0][0] - eValue[0], m[0][1]],
+            [m[1][0], m[1][1] - eValue[0]]
+        ]
+        temp2 = [
+            [m[0][0] - eValue[1], m[0][1]],
+            [m[1][0], m[1][1] - eValue[1]]
+        ]
+
+        ans1 = gauss(temp1, [0, 0])
+        ans2 = gauss(temp2, [0, 0])
+
+        return [ans1, ans2]
+    }
+}
 
 // Transformed grid
 function displayTransformedGrid(m) {
@@ -59,7 +109,7 @@ function displayTransformedGrid(m) {
             let vec = matrixMult(m, [i, j])
             ctx.lineTo(u * vec[0], -u * vec[1])
     }
-    ctx.strokeStyle = "cyan"
+    ctx.strokeStyle = "red"
     ctx.stroke()
     }
 
@@ -69,7 +119,7 @@ function displayTransformedGrid(m) {
             let vec = matrixMult(m, [j, i])
             ctx.lineTo(u * vec[0], -u * vec[1])
     }
-    ctx.strokeStyle = "cyan"
+    ctx.strokeStyle = "red"
     ctx.stroke()
     }
 }
@@ -80,19 +130,52 @@ function temp() {
     let b = [
         [document.getElementById('m00').value, document.getElementById('m01').value],
         [document.getElementById('m10').value, document.getElementById('m11').value]
-    ]    
+    ]   
+    
     displayTransformedGrid(b)
+
+    ctx.lineWidth = 2
+    ctx.beginPath()
+    ctx.lineTo(0, 0)
+    ctx.lineTo(u * b[0][0], -u * b[1][0])
+    ctx.strokeStyle = "darkgray"
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.lineTo(0, 0)
+    ctx.lineTo(u * b[0][1], -u * b[1][1])
+    ctx.strokeStyle = "darkgray"
+    ctx.stroke()
+    ctx.lineWidth = 1
+
+    ctx.beginPath()
+    ctx.lineTo(0, 0)
+    ctx.lineTo(u * b[0][0], -u * b[1][0])
+    ctx.lineTo(u * b[0][0] + u * b[0][1], -u * b[1][0] - u * b[1][1])
+    ctx.lineTo(u * b[0][1], -u * b[1][1])
+    ctx.closePath()
+    ctx.fillStyle = 'rgb(32, 246, 0)'
+    ctx.fill()
+    
+
+    
 }
+
+
+// COPIED GUASSIAN ELIMATION CODE
+
+
+
+// COPIED GAUSSIAN ELIMATION CODE
+
+
 
 displayBaseGrid()
 
-
-
-
-
-
-
-
+m = [
+    [2, 3],
+    [2, 1]
+]
 
 
 
